2d090dade2eb60853bc7b0ab132d153324a6463a
Marcin Zyla <marcin.zyla0@gmail.com>
2017-12-30 01:17:22 +0100

    Memory - add rest of registers set/get

--- src/memory.ts
+++ src/memory.ts
@@ -25,14 +25,6 @@ class Memory {
         this.SP = 0;
     }
 
-    public getPC(): number {
-        return this.PC;
-    }
-
-    public setPC(newPC: number) {
-        this.PC = newPC;
-    }
-
     public getVReg(reg: number): number {
         let index = Math.floor(reg / 4);
         let sub = 3 - reg % 4;
@@ -53,6 +45,40 @@ class Memory {
 
     public setIReg(value: number) {
         this.Iregister = value;
+    }
+
+    public setTimeDelay(val: number) {
+        this.Tregisters = (this.Tregisters | ~(0xFF << 8*3));
+        this.Tregisters = (this.Tregisters | (val << 8*3));
+    }
+
+    public getTimeDelay(): number {
+        return this.Tregisters & (0xFF << 8*3);
+    }
+
+    public setSoundDelay(val: number) {
+        this.Tregisters = (this.Tregisters | ~(0xFF << 8*2));
+        this.Tregisters = (this.Tregisters | (val << 8*2));
+    }
+
+    private getSoundDelay(): number {
+        return this.Tregisters & (0xFF << 8*2);
+    }
+
+    public getPC(): number {
+        return this.PC;
+    }
+
+    public setPC(newPC: number) {
+        this.PC = newPC;
+    }
+
+    public getSP(): number {
+        return this.SP;
+    }
+
+    public setSP(newSP: number) {
+        this.SP = newSP;
     }
 
     public clear_memory() {
