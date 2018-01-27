8d5b869368bb981a7087918194e0c2cf05e62943
Marcin Zyla <marcin.zyla0@gmail.com>
2018-01-27 22:26:54 +0100

    Add loader stub

---
+++ src/loader.ts
@@ -0,0 +1,12 @@
+/// <reference path="memory.ts" />
+class Loader {
+    constructor(mem: Memory) {
+    }
+
+    public load() {
+      // dummy function that loads example program
+    }
+ 
+}
+
+    
